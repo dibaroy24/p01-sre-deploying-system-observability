0b658b61ee9ea07d7a1b4084dc54900b5c16b340
Dibakar Roy <dibakar.roy24@gmail.com>
2024-04-13 11:47:11 -0400

    Updated the reference to undeclared module in ec2.tf file

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web" {
   ami           = var.aws_ami
   instance_type = "t3.micro"
   # subnet_id   = "subnet-0aa7a2a3113c46086"
-  subnet_id     = module.vpc.public_subnet_ids
+  # subnet_id   = module.vpc.public_subnet_ids
+  subnet_id     = var.private_subnet_ids
   key_name = "udacity"
   tags = {
     Name = "ubuntu"
@@ -11,8 +12,8 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
-  # vpc_id    = var.vpc_id
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = var.vpc_id
+  # vpc_id    = module.vpc.vpc_id
 
   ingress {    
     description = "web port"
